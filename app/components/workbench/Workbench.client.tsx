e2c096c5c4105a09c770a9b526ad9b8b2d223e92
francisco <francisco.huesca@cbrands.com>
2025-03-26 16:07:24 -0600

    updated preview url

--- app/components/workbench/Workbench.client.tsx
+++ app/components/workbench/Workbench.client.tsx
@@ -406,8 +406,8 @@ export const Workbench = memo(
                             workbenchStore
                               .pushToStageRepo(projectName, commitMessage, zipFile, workbenchStore.chart)
                               .then(() => {
-                                setPreviewLink(`https://stage-${projectName}.sdlc.app.cbrands.com`)
-                                alert(`Success uploaded to: https://stage-${projectName}.sdlc.app.cbrands.com`);
+                                setPreviewLink(`https://stage-${projectName}.sbx.sdlc.app.cbrands.com`)
+                                alert(`Success uploaded to: https://stage-${projectName}.sbx.sdlc.app.cbrands.com`);
                               })
                               .catch(() => {
                                 alert('There was an error uploading...');
