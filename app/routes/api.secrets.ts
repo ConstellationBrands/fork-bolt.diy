062e25d6c4f6f00e75e25c65f76785ec159207b6
francisco <francisco.huesca@cbrands.com>
2025-04-11 14:09:22 -0600

    integrating argo workflows trigger

--- app/routes/api.secrets.ts
+++ app/routes/api.secrets.ts
@@ -1,7 +1,22 @@
 import { json, type LoaderFunction } from '@remix-run/cloudflare';
 
-export const loader: LoaderFunction = async ({ context }) => { // Access the context object
+export const loader: LoaderFunction = async ({ request, context }) => { // Access the context object
   const githubToken = context.cloudflare.env.GITHUB_TOKEN;
+
+  console.log('REQUEST: ', request.headers.get('cookie'));
+
+  const response = await fetch('https://workflows.devop.sdlc.app.cbrands.com/api/v1/events/enterprise-tool-flow-dev/zip', {
+    method: 'POST',
+    headers: {
+      cookie: request.headers.get('cookie'),
+    },
+    body: JSON.stringify({
+      zipfile_name: 'code.zip',
+    })
+  })
+
+  console.log('RESPONSE', response.status.toString());
+
 
   return json({
     githubToken: githubToken || 'undefined',
