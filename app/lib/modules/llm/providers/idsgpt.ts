baa0bdfb1029556fedfbec882009b49f141bf689
RubenML <ruben.maldonado1@cbrands.com>
2025-05-14 16:38:33 -0600

    update idsgpt max tokens

--- app/lib/modules/llm/providers/idsgpt.ts
+++ app/lib/modules/llm/providers/idsgpt.ts
@@ -43,7 +43,7 @@ export default class IDSGPTProvider extends BaseProvider {
       name: model.id,
       label: model.id,
       provider: this.name,
-      maxTokenAllowed: 32000,
+      maxTokenAllowed: 8000,
     }));
   }
 
