ff6498cff7f1c67dff2fb66cd62eac8723a6f2ee
francisco <francisco.huesca@cbrands.com>
2026-02-18 17:30:25 -0600

    added nextjs template as prefered one

--- app/utils/selectStarterTemplate.ts
+++ app/utils/selectStarterTemplate.ts
@@ -5,7 +5,7 @@ import { STARTER_TEMPLATES } from './constants';
 
 const starterTemplateSelectionPrompt = (templates: Template[]) => `
 You are an experienced developer who helps people choose the best starter template for their projects.
-IMPORTANT: Vite is preferred
+IMPORTANT: NextJS 14 is preferred
 IMPORTANT: Only choose shadcn templates if the user explicitly asks for shadcn.
 
 Available templates:
