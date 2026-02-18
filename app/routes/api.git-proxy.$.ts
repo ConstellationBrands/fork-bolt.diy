4119fc3b75d104f56da1ac5ecb212e38386aebe2
francisco <francisco.huesca@cbrands.com>
2026-02-18 13:20:59 -0600

    debugging headers in platform

--- app/routes/api.git-proxy.$.ts
+++ app/routes/api.git-proxy.$.ts
@@ -109,7 +109,9 @@ async function handleProxyRequest(request: Request, path: string | undefined, co
     if (context.cloudflare.env.GITHUB_USER && context.cloudflare.env.GITHUB_TOKEN) {
       const basicAuth = btoa(`${context.cloudflare.env.GITHUB_USER}:${context.cloudflare.env.GITHUB_TOKEN}`);
       headers.set('Authorization', `Basic ${basicAuth}`);
+      console.log('Set authotization header')
     } else {
+      console.log('Deleting authotization header')
       headers.delete('Authorization');
     }
 
