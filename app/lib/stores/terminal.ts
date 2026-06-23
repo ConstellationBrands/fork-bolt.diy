501f5e7708be947578634e2215bc2ffaf4de9a89
francisco <francisco.huesca@cbrands.com>
2026-06-23 17:44:41 -0600

    fix: terminal issue

--- app/lib/stores/terminal.ts
+++ app/lib/stores/terminal.ts
@@ -9,7 +9,7 @@ export class TerminalStore {
   #terminals: Array<{ terminal: ITerminal; process: WebContainerProcess }> = [];
   #boltTerminal = newBoltShellProcess();
 
-  showTerminal: WritableAtom<boolean> = import.meta.hot?.data.showTerminal ?? atom(false);
+  showTerminal: WritableAtom<boolean> = import.meta.hot?.data.showTerminal ?? atom(true);
 
   constructor(webcontainerPromise: Promise<WebContainer>) {
     this.#webcontainer = webcontainerPromise;
